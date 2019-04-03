b5f0211cc991d5ac8c63a99d05b28e6ce4408c84
Nomeleel <choonlay@outlook.com>
2019-04-03 23:27:51 +0800

    Add pig

---
+++ AngryBird/Assets/Scripts/Pig.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pig : MonoBehaviour
+{
+    public float maxSpeed = 10;
+    public float minSpeed = 5;
+
+    private SpriteRenderer spriteRenderer;
+    public Sprite hurt;
+    public GameObject boom;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print(collision.relativeVelocity.magnitude);
+        if (collision.relativeVelocity.magnitude >= maxSpeed)
+        {
+            Dead();
+        } 
+        else if (collision.relativeVelocity.magnitude < maxSpeed && collision.relativeVelocity.magnitude > minSpeed)
+        {
+            spriteRenderer.sprite = hurt;
+        }
+        else
+        {
+
+        }
+    }
+
+    private void Dead()
+    {
+        Destroy(gameObject);
+        Instantiate(boom, transform.position, Quaternion.identity);
+    }
+
+}
