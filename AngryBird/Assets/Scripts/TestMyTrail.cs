8b6e2ae21c2bbfad46ab9b1c9d4f23a704581e0d
Nomeleel <choonlay@outlook.com>
2019-04-07 12:43:14 +0800

    add trail and other background

---
+++ AngryBird/Assets/Scripts/TestMyTrail.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMyTrail : MonoBehaviour
+{
+    public WeaponTrail myTrail;
+
+    private float t = 0.033f;
+    private float tempT = 0;
+    private float animationIncrement = 0.003f;
+
+    void Start()
+    {
+        myTrail.SetTime(0.0f, 0.0f, 1.0f);
+    }
+
+    void LateUpdate()
+    {
+        t = Mathf.Clamp(Time.deltaTime, 0, 0.066f);
+
+        if (t > 0)
+        {
+            while (tempT < t)
+            {
+                tempT += animationIncrement;
+
+                if (myTrail.time > 0)
+                {
+                    myTrail.Itterate(Time.time - t + tempT);
+                }
+                else
+                {
+                    myTrail.ClearTrail();
+                }
+            }
+
+            tempT -= t;
+
+            if (myTrail.time > 0)
+            {
+                myTrail.UpdateTrail(Time.time, t);
+            }
+        }
+    }
+
+    public void StartTrail()
+    {
+        myTrail.SetTime(2.0f, 0.0f, 1.0f);
+        myTrail.StartTrail(0.5f, 0.4f);
+    }
+
+    public void ClearTrail()
+    {
+        myTrail.ClearTrail();
+    }
+}
